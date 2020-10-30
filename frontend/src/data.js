4fed84750c488f1f0045fee8240157d75b9e2477
Khadijat Adebara <greaterheightscomputer@gmail.com>
2020-10-30 15:41:55 +0100

    initial git in amazona4

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 110,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 20,
+            description: 'lovely high quality product'
+        },
+        {
+            _id: '3',
+            name: 'Lacoste Free Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 90,
+            brand: 'Lacoste',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'good product'
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 290,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'high quality pant'
+        },
+        {
+            _id: '5',
+            name: 'Puma Slim Pant',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 89,
+            brand: 'Puma',
+            rating: 4.2,
+            numReviews: 21,
+            description: 'high quality product'
+        },
+        {
+            _id: '6',
+            name: 'Adidas Slim Pant',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 139,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 15,
+            description: 'high quality product'
+        }
+    ]
+}
+
+export default data;
